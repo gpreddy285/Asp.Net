8adb67f37378e7bd123d19c6475ab00dce573438
gpreddy <prasad1annareddy@gmail.com>
2026-04-21 15:23:50 +0530

    code updated

---
+++ newasp/viewstate.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="viewstate.aspx.cs" Inherits="newasp.viewstate" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="label1" runat="server"></asp:Label><br />
+            <asp:Button ID="button1" runat="server" Text="submit" OnClick="button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ newasp/viewstate.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace newasp
+{
+    public partial class viewstate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                ViewState["clickscount"]= 0;
+            }
+        }
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            int count = (int)ViewState["clickscount"];
+            count++;
+            ViewState["clickcount"] = count;
+            label1.Text = "click count" + count;
+        }
+    }
+}
